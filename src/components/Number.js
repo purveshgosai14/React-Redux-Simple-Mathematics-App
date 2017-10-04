b91f1a12568adba462ef7b82e001e7e28678e67b
Purvesh Gosai <puri.ui369@gmail.com>
2017-10-04 16:54:04 -0500

    Create number component

---
+++ src/components/Number.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import { Button } from 'react-bootstrap';
+
+class Number extends Component {
+    render() {
+        return(
+            <div>
+            <div className="row col-sm-offset-5">
+                <p>0</p>
+            </div>
+                <div className="row col-sm-offset-4">
+                    <Button bsStyle="primary">Increment</Button>
+            </div>
+            </div>
+        )
+    }
+}
+
+export default Number;
