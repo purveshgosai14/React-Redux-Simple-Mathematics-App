1136b2b5c503ac113d9ace394154f4c8361b0568
Purvesh Gosai <puri.ui369@gmail.com>
2017-10-05 01:54:24 -0500

    Add state methods

--- src/components/Number.js
+++ src/components/Number.js
@@ -2,16 +2,20 @@ import React, { Component } from 'react';
 import { Button } from 'react-bootstrap';
 
 import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+
+import { incrementNumber } from '../actions/index'
 
 class Number extends Component {
     render() {
+        console.log('from the props', this.props);
         return(
             <div>
             <div className="row col-sm-offset-5">
-                <p>0</p>
+                <p>{this.props.num}</p>
             </div>
                 <div className="row col-sm-offset-4">
-                    <Button bsStyle="primary">Increment</Button>
+                    <Button bsStyle="primary" onClick={()=>this.props.incrementNumber(this.props.num)}>Increment</Button>
             </div>
             </div>
         )
@@ -21,8 +25,12 @@ class Number extends Component {
 function mapStateToProps(state){
     console.log('mapStateToProps - Number.js', state);
     return{
-    num:state.num,
+    num: state.num
     }
 }
 
-export default connect(mapStateToProps, null)(Number);
+function mapDispatchToProps(dispatch){
+    return bindActionCreators({incrementNumber}, dispatch);
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Number);
