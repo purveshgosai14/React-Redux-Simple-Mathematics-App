0d549634ae4aecb03386df6f043d8f571c0f3d22
Purvesh Gosai <puri.ui369@gmail.com>
2017-10-05 01:56:04 -0500

    Bug Fix: Increment number

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,11 +1,11 @@
-import { INC_NUMBER } from "../actions/index"
+import { INC_NUMBER } from '../actions/index'
 
 function performAction(state = { num: 0}, action) {
     switch(action.type){
         case INC_NUMBER:
             console.log('Action performed from the reducer');
             console.log(state, action);
-            let num =0;
+            let num = state.num+1;
             return { num };
         default:
             return state;
