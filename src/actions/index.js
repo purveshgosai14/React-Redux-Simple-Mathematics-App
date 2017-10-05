8f7d1a18cd4eb4d4de4db8bbe4244afff0888047
Purvesh Gosai <puri.ui369@gmail.com>
2017-10-05 14:33:45 -0500

    Add function for decrement num

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,8 +1,17 @@
 export const INC_NUMBER= "INC_NUMBER";
+export const DEC_NUMBER= "DEC_NUMBER";
 
 export function incrementNumber(num) {
     const action = {
         type: INC_NUMBER,
+        num
+    };
+    return action;
+}
+
+export function decrementNumber(num) {
+    const action = {
+        type: DEC_NUMBER,
         num
     };
     return action;
